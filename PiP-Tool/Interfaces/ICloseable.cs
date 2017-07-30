b80775cf44730b5cb2ee3d0a5ff1f572dd915c32
Lionel Jouin <lionel.jouin@outlook.com>
2017-07-30 21:33:38 +0200

    Creating the Windows service

--- PiP-Tool/Interfaces/ICloseable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace PiP_Tool.Interfaces
-{
-    public interface ICloseable
-    {
-        event EventHandler<EventArgs> RequestClose;
-    }
-}
---
+++ PiP-Tool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiP_Tool
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}

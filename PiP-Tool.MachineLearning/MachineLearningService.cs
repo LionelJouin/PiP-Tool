934fdecefc11993699806d9686e14e1eec9689fe
Lionel Jouin <lionel.jouin@outlook.com>
2018-07-06 21:46:22 +0200

    Init ML project (with ML.NET)

---
+++ PiP-Tool.MachineLearning/MachineLearningService.cs
@@ -0,0 +1,34 @@
+﻿namespace PiP_Tool.MachineLearning
+{
+    public class MachineLearningService
+    {
+
+        #region public
+
+        /// <summary>
+        /// Gets the instance of the singleton
+        /// </summary>
+        public static MachineLearningService Instance => _instance ?? (_instance = new MachineLearningService());
+
+        #endregion
+
+        #region private
+
+        private static MachineLearningService _instance;
+
+        #endregion
+
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~MachineLearningService() => Dispose();
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+    }
+}
